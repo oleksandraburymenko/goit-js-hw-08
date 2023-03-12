0577c4f18d692c3bbf4778da82418d13cb6838f1
oleksandraburymenko <115561772+oleksandraburymenko@users.noreply.github.com>
2023-03-12 19:33:08 +0200

    Third task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,7 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(function() {
+player.on('timeupdate', throttle( function () {
     localStorage.setItem('videoplayer-current-time', player.getCurrentTime());
   }, 1000)());
 
@@ -12,4 +12,4 @@ const currentTime = localStorage.getItem('videoplayer-current-time');
 
 if (currentTime) {
   player.setCurrentTime(currentTime);
-}
+};
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+
+const stateKey = 'feedback-form-state';
+
+formEl.addEventListener('input', throttle(e => {
+    const state = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+    localStorage.setItem(stateKey, JSON.stringify(state));
+  }, 500));
+
+const savedState = localStorage.getItem(stateKey);
+if (savedState) {
+    const state = JSON.parse(savedState);
+    emailInput.value = state.email;
+    messageInput.value = state.message;
+}
+
+formEl.addEventListener('submit', e => {
+    e.preventDefault();
+    const state = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+    console.log(state);
+    localStorage.removeItem(stateKey);
+    emailInput.value = '';
+    messageInput.value = '';
+  });
