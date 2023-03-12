fee5e075637dcc6367c15daa80c014bc30d7fb2a
oleksandraburymenko <115561772+oleksandraburymenko@users.noreply.github.com>
2023-03-12 19:57:17 +0200

    Add changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,12 +4,13 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(function() {
-    localStorage.setItem('videoplayer-current-time', player.getCurrentTime());
-}, 1000));
+player.on('timeupdate', _.throttle(() => {
+    const currentTime = player.currentTime();
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  }, 1000));
 
 const currentTime = localStorage.getItem('videoplayer-current-time');
 
 if (currentTime) {
   player.setCurrentTime(currentTime);
-};
+}
