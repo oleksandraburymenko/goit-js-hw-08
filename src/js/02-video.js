9784df0c94a18e6e36551a9dc4a4f03e790da03d
oleksandraburymenko <115561772+oleksandraburymenko@users.noreply.github.com>
2023-03-12 17:17:49 +0200

    Second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(function() {
+    localStorage.setItem('videoplayer-current-time', player.getCurrentTime());
+  }, 1000)());
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
