74221982f2d7a41dbdbc32293954543e4636b236
oleksandraburymenko <115561772+oleksandraburymenko@users.noreply.github.com>
2023-03-13 20:54:52 +0200

    Add changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,16 +2,25 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+const script = document.createElement('script');
+script.src = 'https://player.vimeo.com/api/player.js';
+document.body.appendChild(script);
 
-player.on('timeupdate', throttle(() => {
-  const currentTime = player.currentTime();
-  localStorage.setItem('videoplayer-current-time', currentTime);
-}, 1000));
+script.onload = () => {
+  const player = new Player(iframe);
 
-player.on('ready', () => {
-  const currentTime = localStorage.getItem('videoplayer-current-time');
-  if (currentTime) {
-    player.setCurrentTime(currentTime);
-  }
-});
+  player.on('timeupdate', throttle(() => {
+    const readyPlayer = player.getReady();
+    if (readyPlayer) {
+      const currentTime = player.currentTime();
+      localStorage.setItem('videoplayer-current-time', currentTime);
+    }
+  }, 1000));
+
+  player.on('ready', () => {
+    const currentTime = localStorage.getItem('videoplayer-current-time');
+    if (currentTime) {
+      player.setCurrentTime(currentTime);
+    }
+  });
+};
